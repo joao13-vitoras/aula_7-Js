79cee3fbef8b9b7f98b0f0ba22d9578907657568
joao13-vitoras <70807079+joao13-vitoras@users.noreply.github.com>
2020-10-19 13:02:14 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+const ano_atual = "2020"
+const ano_nascimento = prompt('Digite seu ano de nascimento:')
+const nome_usuario = prompt('Digite seu nome:')
+parseInt(ano_atual)
+parseInt(ano_nascimento)
+
+let idade
+idade = ano_atual - ano_nascimento
+
+alert( `Olá, seu nome é ${nome_usuario} e você tem ${idade} anos`)
+
